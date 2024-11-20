91f56df7d0d163fe5d7c1c66913b53718f0c11d1
Shiffat Singh <saizen777999@gmail.com>
2024-11-20 19:26:17 +0530

    Update order.route.js

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -4,8 +4,8 @@ import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
-router.get("/", verifyToken, getOrders);
-router.post("/create-payment-intent/:id", verifyToken, intent);
-router.put("/", verifyToken, confirm);
+router.get("/", getOrders);
+router.post("/create-payment-intent/:id", intent);
+router.put("/", confirm);
 
 export default router;
